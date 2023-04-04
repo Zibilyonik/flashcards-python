1c2f1165035bbf3a32fb050b8566083c8f94029b
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-04-04 14:21:21 +0200

    restructurised code to have separate ask_cards and add_card functions, added card ask loop

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 #python3
 
-front = input()
-back = input()
-answer = input()
-if back == answer:
-    print("Your answer is right!")
-else:
-    print("Your answer is wrong...")
-    
+
+def add_card(cards_list, card_number):
+    print(f"The term for card #{card_number}:")
+    front = str(input())
+    print(f"The definition for card #{card_number}:")
+    back = str(input())
+    cards_list[front] = back
+
+def ask_cards(cards_list):
+    for i in cards_list:
+        print(f"print the definition of \"{i}\"):")
+        answer = str(input())
+        if answer == cards_list[i]:
+            print("Correct!")
+        else:
+            print(f"Wrong. The right answer is \"{cards_list[i]}\".")
+            
+def main():
+    cards_list = {}
+    print("Input the number of cards:")
+    count = input()
+    for i in range(int(count)):
+        add_card(cards_list, i + 1)
+    ask_cards(cards_list)
+
+if __name__ == "__main__":
+    main()
